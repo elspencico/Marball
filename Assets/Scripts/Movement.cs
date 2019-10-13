9abb4214e1e67a7a44aec3c50be977d36a9eff5a
elspencico <43080321+elspencico@users.noreply.github.com>
2019-10-12 20:26:02 -0500

    Added Movement and unfinished camera implementation

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float force = 500f;
+
+   
+    void FixedUpdate()
+    {
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(force * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-force * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, force * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -force * Time.deltaTime);
+        }
+    }
+}
--- Assets/Scripts/rotationlock.cs
+++ Assets/Scripts/rotationlock.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gravity : MonoBehaviour
+public class rotationlock : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,7 @@ public class Gravity : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.rotation = Quaternion.Euler(transform.rotation.x, 0, transform.rotation.z);
+
     }
 }
